8fea47b9ca6b03263d0a6abc29cad814c8f57710
Nahiyan Nashrah(2016831012) <42367131+NahiyanNashrah@users.noreply.github.com>
2020-01-12 12:38:04 +0600

    implementing Roll Dice button
    generating random number and displaying the dice

--- DOM/app.js
+++ DOM/app.js
@@ -9,14 +9,54 @@ GAME RULES:
 
 */
 
-var scores,roundScore, activePlayer,dice;
+var scores,roundScore, activePlayer ;
 
 scores = [0,0];
 roundscore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-document.querySelector('#current-0').textContent = dice;
+//document.querySelector('#current-'+activePlayer).textContent = dice;
+//document.querySelector('#current-'+activePlayer).innerHTML = '<em>' + dice + '</em>';
 
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+
+//Callback function : passing the function as a parameter and that function will be called by the function 
+
+/* 
+function btn(){
+
+} 
+
+document.querySelector('.btn-roll').addEventListener('click',btn)
+
+*/
+
+
+//Anonymous Function : A function that doesn't have any name and can't be reused.
+
+/*
+document.querySelector('.btn-roll').addEventListener('click',function(){
+});
+*/
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+    //1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    
+
+
+});
