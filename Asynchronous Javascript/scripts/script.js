63ce551ef6725db74530cb997906766a0389c191
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-01-26 11:25:55 +0600

    practice promis

--- Asynchronous Javascript/scripts/script.js
+++ Asynchronous Javascript/scripts/script.js
@@ -1,36 +1,35 @@
-// var a = 1;
-// function getRecipe(){
-//     setTimeout( () => {
-//         const recipeID = [143,523,150,177];
-//         console.log(recipeID);
-//         setTimeout(id => {
-//             const recipe = {
-//                 title: 'Fried rice',
-//                 producer : 'john'
-//             };
-//             console.log(`${id}: ${recipe.title}`);
+function getRecipe(){
+    setTimeout( () => {
+        const recipeID = [143,523,150,177];
+        console.log(recipeID);
+        setTimeout(id => {
+            const recipe = {
+                title: 'Fried rice',
+                producer : 'john'
+            };
+            console.log(`${id}: ${recipe.title}`);
 
-//             setTimeout(publisher => {
-//                 const recipe2 = {
-//                     title: 'Chicken Fry',
-//                     producer : 'john'
-//                 }
-//                 console.log(recipe2);
-//             }, 1500 ,recipe.producer);
+            setTimeout(publisher => {
+                const recipe2 = {
+                    title: 'Chicken Fry',
+                    producer : 'john'
+                }
+                console.log(recipe2);
+            }, 1500 ,recipe.producer);
 
-//         }, 1500, recipeID[2]);
+        }, 1500, recipeID[2]);
 
-//     }, 1500);
-// }
+    }, 1500);
+}
 
 console.log(`before function is invoked `);
-//getRecipe();
+getRecipe();
 console.log(`after function is invoked `);
 
 //Promise
 const getId = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve([143,523,150,177]);
+        resolve([143,523,150]);
     }, 1500);
 });
 const getRecipe = recID => {
@@ -60,8 +59,8 @@ const getPublisher = publisher => {
 getId
 .then(IDs => {
     console.log(IDs);
-    return getRecipe(IDs[2]);
-})
+    //return getRecipe(IDs[2]);
+ });
 .then(recipe => {
     console.log(recipe);
     return getPublisher('john');
@@ -72,3 +71,28 @@ getId
 .catch(error => {
     console.log(`error`);
 });
+
+
+let getInput = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve([3,6]);
+    },1500);
+});
+getInput.then(sum);
+
+function sum(arr){
+    let total = 0;
+    arr.forEach(element => {
+        total += element;
+    });
+    console.log(total);
+}
+
+new Promise(function (resolve, reject) {
+    //setTimeout(() => {
+        throw new Error('Whoops!');
+       // reject(new Error('Error'));
+   // },1000);
+}).catch(alert);
+
+
