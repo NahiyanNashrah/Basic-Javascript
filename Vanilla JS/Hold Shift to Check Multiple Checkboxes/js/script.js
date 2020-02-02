8592679e3de62f093a8832e68ed2b18ba4c372e1
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-02 15:01:04 +0600

    remove input from queryselector

--- Vanilla JS/Hold Shift to Check Multiple Checkboxes/js/script.js
+++ Vanilla JS/Hold Shift to Check Multiple Checkboxes/js/script.js
@@ -1,4 +1,4 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxes = document.querySelectorAll('[type="checkbox"]');
 let lastChecked;
 function handleCheck(e){
     let inBetween = false;
@@ -7,7 +7,6 @@ function handleCheck(e){
             console.log(box);
             if(box === this || box === lastChecked){
                 inBetween = !inBetween;
-                console.log(`Starting to check`);
             }
             if(inBetween)
             box.checked = true;
