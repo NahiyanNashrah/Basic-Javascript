f89e081f68d1218a5ae3d0d45edae35bb4525833
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-05 12:34:25 +0600

    added some comments

--- Vanilla JS/Update CSS variables with JS/js/script.js
+++ Vanilla JS/Update CSS variables with JS/js/script.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate(){
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}` ,this.value + suffix);
+    document.documentElement.style.setProperty(`--${this.name}` ,this.value + suffix); //--${this.name} => --spacing / --blur /--base
 }
 
 inputs.forEach(input => {
